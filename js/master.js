dd9cb442c930aadda43230f71464bc3592f0f618
Amr Safwat <amrsafwat02@gmail.com>
2023-04-08 02:22:15 +0200

    Upload The Design And Created Landing Page

---
+++ js/master.js
@@ -0,0 +1,13 @@
+let background = document.querySelector('.landing-page');
+let image = new Image();
+
+let imgs = [];
+
+for(let i = 1; i<= 5;i++) {
+  imgs.push(`../imgs/0${i}.jpg`);
+}
+
+setInterval(() => {
+  background.style.backgroundImage = `url('${imgs[Math.floor(Math.random() * imgs.length)]}')`;
+}, 60000);
+
