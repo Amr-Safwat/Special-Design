d7587042dddbd607428c337337efac75839184ce
Amr Safwat <amrsafwat02@gmail.com>
2023-04-09 23:15:40 +0200

    Created A Colors Option

--- js/master.js
+++ js/master.js
@@ -1,3 +1,14 @@
+// Check If There Is Local Storage Color
+if(localStorage.color !== null) {
+  document.documentElement.style.setProperty('--main-color',localStorage.color);
+  // Remove Active Class
+  document.querySelectorAll('.colors-list li').forEach((li)=>{
+    li.classList.remove('active');
+    // check If Li Equal Local Storage Item
+    li.dataset.color=== localStorage.color? li.classList.add('active'):'';
+  });
+}
+
 let settingsBox = document.querySelector('.settings-box');
 let icon = document.querySelector('.icon');
 
@@ -5,6 +16,25 @@ icon.onclick = function() {
   settingsBox.classList.toggle('open');
   this.classList.toggle('fa-spin')
 }
+
+// Switch Colors
+let colorsLi = document.querySelectorAll('.colors-list li');
+
+colorsLi.forEach(li => {
+  li.addEventListener('click',(e)=>{
+    // Remove Active Class
+    colorsLi.forEach((li)=>{
+      li.classList.remove('active');
+    });
+    // Add An Active Class On The Clicked Element
+    e.target.classList.add('active');
+    // Set Color On Root
+    document.documentElement.style.setProperty('--main-color',e.target.dataset.color);
+
+    // Store Color In Local Storage
+    localStorage.setItem('color',e.target.dataset.color);
+  })
+});
 
 let background = document.querySelector('.landing-page');
 let image = new Image();
